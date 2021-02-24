58e0b5621cf2a9fbd019bee0b3c0b3526ca2135c
Franek <poczta.f@protonmail.com>
2021-02-24 14:34:20 +0100

    Show LoadingButton on the screen

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -3,8 +3,11 @@ package com.udacity
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.PointF
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
@@ -13,21 +16,38 @@ class LoadingButton @JvmOverloads constructor(
     private var widthSize = 0
     private var heightSize = 0
 
+    private val labelPosition = PointF(0.0f, 0.0f)
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        textAlign = Paint.Align.CENTER
+        textSize = resources.getDimensionPixelSize(R.dimen.default_text_size).toFloat()
+    }
+
     private val valueAnimator = ValueAnimator()
 
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
 
     }
 
+    private var beforeLoadBackground = 0
+    private var afterLoadBackground = 0
 
     init {
+        isClickable = true
 
+        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
+            beforeLoadBackground = getColor(R.styleable.LoadingButton_beforeLoadBackground, 0)
+            afterLoadBackground = getColor(R.styleable.LoadingButton_afterLoadBackground, 0)
+        }
     }
 
 
-    override fun onDraw(canvas: Canvas?) {
+    override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
+        canvas.drawColor(beforeLoadBackground)
+        val label = resources.getString(R.string.app_name)
+        canvas.drawText(label, labelPosition.x, labelPosition.y, paint)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -41,6 +61,9 @@ class LoadingButton @JvmOverloads constructor(
         widthSize = w
         heightSize = h
         setMeasuredDimension(w, h)
+
+        labelPosition.x = (widthSize / 2).toFloat()
+        labelPosition.y = heightSize / 2 - (paint.descent() + paint.ascent()) / 2
     }
 
 }
